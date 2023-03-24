50f384d456e8ba99a1cb43433b31498beebc0412
SimoneS86 <simone.sensini.86@gmail.com>
2023-03-24 09:27:01 +0100

    possibile inizio giornata

--- assets/js/indexScript.js
+++ assets/js/indexScript.js
@@ -1,3 +1,51 @@
+const URLParams = new URLSearchParams(window.location.search);
+const selectedId = URLParams.get("id");
+
+window.onload = async () => {
+  if (selectedId) {
+  }
+};
+
+function showSearch() {
+  let searchDiv = document.getElementById("searchDiv");
+  searchDiv.style.display = "block";
+}
+
+const searchInput = document.getElementById("searchInput");
+searchInput.addEventListener("keydown", async function (event) {
+  if (event.key === "Enter") {
+    const searchText = searchInput.value;
+    const searchUrl = `https://striveschool-api.herokuapp.com/api/deezer/search?q=${searchText}`;
+    try {
+      const response = await fetch(searchUrl);
+      const data = await response.json();
+      console.log(data);
+      console.log(data.data[0]);
+      if (data) {
+        const firstResult = data.data[0];
+        if (searchText.toLowerCase() === firstResult.artist.name.toLowerCase()) {
+          window.location.href = `artist.html?id=${firstResult.artist.id}`;
+        } else if (searchText.toLowerCase() === firstResult.album.title.toLowerCase()) {
+          window.location.href = `album.html?id=${firstResult.album.id}`;
+        } else if (
+          searchText.toLowerCase() === firstResult.title.toLowerCase() ||
+          searchText.toLowerCase() === firstResult.title_short.toLowerCase()
+        ) {
+          window.location.href = `index.html?id=${firstResult.id}`;
+        } else {
+          console.log(
+            `Non hai cercato né l'artista ${firstResult.artist.name}, né l'album ${firstResult.album.title}, ma "${searchText}"`
+          );
+        }
+      } else {
+        console.log("Nessun risultato trovato!");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+});
+
 // const audio = document.getElementById("myAudio");
 // const playPauseBtn = document.getElementById("playPauseBtn");
 // let isPlaying = false;
